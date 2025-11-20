f53f26e1d8a50d85d2a82f09fab4a4bcab5b1298
ogkunta666 <markizoli06@gmail.com>
2025-11-20 10:31:11 +0100

    qwe

--- app/Http/Controllers/Api/ReservationController.php
+++ app/Http/Controllers/Api/ReservationController.php
@@ -8,34 +8,53 @@ use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $user = $request->user();
+        if ($user->isAdmin){
         $reservations = Reservation::all();
+        } else {
+            $reservations = Reservation::where('user_id', $user->id)->get();
+        }
+
         return response()->json($reservations, 200);
     }
 
-    public function show($id){
+    public function show(Request $request, $id){
+        $user = $request->user();
         $reservation = Reservation::findOrFail($id);
-        if ($reservation){
-            return response()->json($reservation, 200);
+        
+        if(!$user->is_admin && $reservation -> user_id!=$user->id){
+            return response()->json(['message' => 'Nincs jogosultságod megtekinteni ezt a foglalást!'], 403);
         }
+        
+        return response()->json($reservation, 200);
+        
         return response()->json(['message' => 'Hiba! A foglalás nem található!'], 404);
     }
 
     public function store(Request $request){
         $validated = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email',
             'reservation_time' => 'required|date',
             'guests' => 'required|integer|min:1',
             'note' => 'nullable|string',
         ]);
-        $reservation = Reservation::create($validated);
+        $reservation = Reservation::create([
+            'user_id' => $request->user()->id,
+            'reservation_time' => $validated['reservation_time'],
+            'guests' => $validated['guests'],
+            'note' => $validated['note'] ?? null,
+        ]);
         return response()->json($reservation, 201);
     }
 
     public function update(Request $request, $id){
+        $user = $request->user();
         $reservation = Reservation::findOrFail($id);
         
+        if(!$user->is_admin && $reservation -> user_id!=$user->id){
+            return response()->json(['message' => 'Nincs jogosultságod módosítani ezt a foglalást!'], 403);
+        }
+
         $validated = $request->validate([
             'name' => 'sometimes|required|string',
             'email' => 'sometimes|required|email',
@@ -48,8 +67,13 @@ class ReservationController extends Controller
         return response()->json($reservation, 200);
     }
 
-    public function destroy($id){
+    public function destroy(Request $request, $id){
+        $user = $request->user();
         $reservation = Reservation::findOrFail($id);
+
+        if(!$user->is_admin && $reservation -> user_id!=$user->id){
+            return response()->json(['message' => 'Nincs jogosultságod törölni ezt a foglalást!'], 403);
+        }
 
         if ($reservation){
             $reservation->delete();
--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,17 @@ Route::post('/register',[App\Http\Controllers\AuthController::class, 'register']
 Route::post('/login',[App\Http\Controllers\AuthController::class, 'login']);
 
 //Authenticated routes
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout',[App\Http\Controllers\AuthController::class, 'logout']);
 
+/*
+Route::middleware('auth:sanctum')->get('/reservations',[ReservationController::class, 'index']); // összes foglalás
+Route::middleware('auth:sanctum')->get('/reservations/{id}',[ReservationController::class, 'show']); // egy foglalás
+Route::middleware('auth:sanctum')->post('/reservations',[ReservationController::class, 'store']); // egy foglalás rögzítése
+Route::middleware('auth:sanctum')->put('/reservations/{id}',[ReservationController::class, 'update']); // egy foglalás minden mezőjét módosítom
+Route::middleware('auth:sanctum')->patch('/reservations/{id}',[ReservationController::class, 'update']); // egy foglalás néhány mezőjét módosítom
+Route::middleware('auth:sanctum')->delete('/reservations/{id}',[ReservationController::class, 'destroy']); // egy foglalás törlése
+*/
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[ReservationController::class, 'logout']);
+    Route::apiResource('reservations', ReservationController::class);
+    
 });
-Route::get('/reservations',[ReservationController::class, 'index']); // összes foglalás
-Route::get('/reservations/{id}',[ReservationController::class, 'show']); // egy foglalás
-Route::post('/reservations',[ReservationController::class, 'store']); // egy foglalás rögzítése
-Route::put('/reservations/{id}',[ReservationController::class, 'update']); // egy foglalás minden mezőjét módosítom
-Route::patch('/reservations/{id}',[ReservationController::class, 'update']); // egy foglalás néhány mezőjét módosítom
-Route::delete('/reservations/{id}',[ReservationController::class, 'destroy']); // egy foglalás törlése
